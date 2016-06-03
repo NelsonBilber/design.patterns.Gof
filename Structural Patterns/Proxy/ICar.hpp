4ccaa13244f8447b52ace48c0680058c68ea3fb3
Nelson Rodrigues <nrodrigues@abyssal.eu>
2016-06-03 18:07:32 +0100

    structural patterns: proxy

---
+++ Structural Patterns/Proxy/ICar.hpp
@@ -0,0 +1,11 @@
+#ifndef ICar_h
+#define ICar_h
+
+#include <iostream>
+
+class ICar{
+public:
+	virtual void drive() = 0;
+};
+
+#endif 
---
+++ Structural Patterns/Proxy/README.md
@@ -0,0 +1,3 @@
+#Proxy
+
+Adds a level of indirection for most complex tasks. Is works as an interface for something else.

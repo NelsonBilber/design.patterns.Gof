c58213c8ee7047bb43be6c3cfd89444ed1ceb15a
Nelson Rodrigues <nrodrigues@abyssal.eu>
2016-06-03 16:35:44 +0100

    structural patterns: facade

---
+++ Structural Patterns/Facade_cpp/SubSystem02.hpp
@@ -0,0 +1,11 @@
+#ifndef SubSystem02_h
+#define SubSystem02_h
+
+#include <iostream>
+
+class SubSystem02 {
+public:
+	void startSystem() { std::cout << "start subsysem 02" << std::endl; }
+};
+
+#endif 
